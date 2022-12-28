ccf2732a4b45f28f42b677c3f1d15cd421c4b68d
Prit-Shah <shahprit8866@gmail.com>
2022-12-28 11:18:12 +0530

    Added two files hello and good

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Hellopage from './comp/Hellopage';
+import Goodmorning from './comp/Goodmorning';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Hellopage></Hellopage>}></Route>
+      <Route path="Good" element={<Goodmorning></Goodmorning>}></Route>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
