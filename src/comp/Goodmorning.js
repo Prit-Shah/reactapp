0605774772ad3ddafd0d6790cd7ed1fffeac7f1e
Prit-Shah <shahprit8866@gmail.com>
2022-12-28 17:18:42 +0530

    mend

--- src/comp/Goodmorning.js
+++ src/comp/Goodmorning.js
@@ -1,20 +1,18 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 function Goodmorning() {
-
-    const[count,setcount]=useState(1);   
-       
-    useEffect(() => {
-        const newint=setInterval(()=>{
-            setcount(count+1)
-        },500);
-        return()=>clearInterval(newint)
-  } ,[count])
-    return (
-        <div>
-            <h1>Good Morning {count}</h1>
-        </div>
-    )
+  const [count, setcount] = useState(1);
+  useEffect(() => {
+    const newint = setInterval(() => {
+      setcount(count + 1);
+    }, 500);
+    return () => clearInterval(newint);
+  }, [count]);
+  return (
+    <div>
+      <h1>Good Morning {count}</h1>
+    </div>
+  );
 }
 
-export default Goodmorning
+export default Goodmorning;
