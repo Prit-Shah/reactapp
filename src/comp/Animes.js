d1aea45c58c13ae3ced8c4994d75f369c3f3b5bf
Prit-Shah <shahprit8866@gmail.com>
2022-12-28 16:46:25 +0530

    working anime and counters

--- src/comp/Animes.js
+++ src/comp/Animes.js
@@ -1,52 +1,75 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
-function Animes() {    
-    const [img, setimg] = useState('');
-    function* generaterandom(count){
-        while(true){
-            yield Math.floor(Math.random() * count);
-        }    
-    }  
-    let newimage="";
-    const getdata=async()=>{
-        let data =await axios.get(`https://api.jikan.moe/v4/characters/${randomid.next().value}`)            
-        return data.data.data;
-    }  
-    useEffect(()=>{
-        let getimage=setTimeout(()=>{
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {LinearProgress,Typography} from '@mui/joy';
+function Animes() {
+  const [img, setimg] = useState("");
+  const [count, setcount] = useState(0);
+  function* generaterandom(count) {
+    while (true) {
+      yield Math.floor(Math.random() * count);
+    }
+  }  
+  useEffect(() => {  
+    const getcount = setInterval(async() => {
+        if(count==5){
+            let data = await axios.get(
+                `https://api.jikan.moe/v4/characters/${randomid.next().value}`
+              );
+              console.log(data.data.data);
+              if (data.data.data.images.jpg.image_url)
+                setimageurl(data.data.data.images.jpg.image_url);
+        } 
+        if(count>5) setcount(0);       
+      setcount(count + 1)
+    }, 500);
 
-        },2000)
-        return()=>clearTimeout(getimage);
-    })
-    const start=()=>{
-        newimage = setInterval(async()=>{
-            let data =await getdata();      
-            console.log(data);            
-            setimageurl(data.images.jpg.image_url)
-        },10000) 
+   return ()=> {clearInterval(getcount);
+        
+    if (count == 5) setcount(0)};
+  }, [img, count]);
+
+  const setimageurl = (data) => {
+    try {
+      setimg(data);
+    } catch (e) {
+      console.error(e);
     }
-    
-    const setimageurl=(data)=>{
-        try{
-            setimg(data)
-        }
-        catch(e){
-            console.error(e);
-        }
-       
-    }  
-    const stop=()=>{
-        clearInterval(newimage);
-    }
-    const totalchars=1000;
-    let randomid=generaterandom(totalchars);    
-    return (
-        <div>
-             <button onClick={()=>{start();console.log("starting interval")}}>start</button><br></br>
-            <button onClick={()=>{stop();console.log("stopping interval")}}>Stop</button><br></br>
-            <img src={img}></img>
-        </div>
-    )
+  };
+
+  const totalchars = 1000;
+  let randomid = generaterandom(totalchars);
+  return (
+    <div className="container">
+        <div style={{width:'225px',marginTop:'20px',marginBottom:'30px'}}>
+        <LinearProgress
+      determinate
+      variant="outlined"
+      color="neutral"
+      size="lg"
+      thickness={32}
+      value={(count*20)}
+      sx={{
+        '--LinearProgress-radius': '0px',
+        '--LinearProgress-progressThickness': '24px',
+        boxShadow: 'sm',
+        borderColor: 'neutral.500',
+      }}
+    >
+      <Typography
+        level="body3"
+        fontWeight="xl"
+        textColor="common.white"
+        sx={{ mixBlendMode: 'difference' }}
+      >
+        LOADING… {`${Math.round(count*20)}%`}
+      </Typography>
+    </LinearProgress></div>
+      {/* <h4>{count}</h4> */}
+      {/* <button onClick={()=>{start();console.log("starting interval")}}>start</button><br></br> */}
+      {/* <button onClick={()=>{stop();console.log("stopping interval")}}>Stop</button><br></br> */}
+      <img src={img}></img>
+    </div>
+  );
 }
 
-export default Animes
+export default Animes;
