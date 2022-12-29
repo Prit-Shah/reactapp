1984ed66554de7a6471b0cc6c7edd5e3f9e9b86d
Prit-Shah <shahprit8866@gmail.com>
2022-12-29 12:14:55 +0530

    from navbar to Material UI Appbar

--- src/comp/Navbar.js
+++ src/comp/Navbar.js
@@ -1,5 +1,31 @@
-import React from 'react'
-import {BrowserRouter,NavLink,Link} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, NavLink, Link, useNavigate } from "react-router-dom";
+import { styled, alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
 
 function Navbar() {
     return (
