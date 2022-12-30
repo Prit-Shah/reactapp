4f712866ec0c3959b53d5540f40cf5fc188c38bd
Prit-Shah <shahprit8866@gmail.com>
2022-12-30 11:24:07 +0530

    changes by prit

--- src/comp/Pageprabhat.js
+++ src/comp/Pageprabhat.js
@@ -1,23 +1,13 @@
 import React from "react";
 
 function Pageprabhat() {
-  return (
-    <div>
-      <h2>Hello, I'm Prabhat</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod,
-        aspernatur repellat! Harum natus perferendis laboriosam dolor a minima,
-        sunt adipisci dolorem nesciunt, aut nam nulla id ipsum odio nemo
-        laborum.
-      </p>
-      <pre>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod,
-        aspernatur repellat! Harum natus perferendis laboriosam dolor a minima,
-        sunt adipisci dolorem nesciunt, aut nam nulla id ipsum odio nemo
-        laborum.
-      </pre>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Prabhat page</h1>
+            <h2>Hello from prabhat with sdfhhdsfjkb</h2>
+            <p>This are some changes made from prit to make conflicts</p>
+        </div>
+    )
 }
 
 export default Pageprabhat;
